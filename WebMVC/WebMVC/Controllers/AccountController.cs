80a7723004450088744eb5cf7730d477099df889
13520387252@163.com <13520387252@163.com>
2017-11-01 17:44:35 +0800

    登陆首页制作【2017年11月1日】

---
+++ WebMVC/WebMVC/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UserLogin(string LoginName,string Password)
+        {
+
+            return View();
+        }
+
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UserRegister()
+        {
+
+            return View();
+        }
+    }
+}
